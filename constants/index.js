914987649494668b32131de7d35623023d41f988
bonnv79 <ngovanbon16@gmail.com>
2022-06-13 16:59:14 +0700

    fix-bug: fix img size

--- constants/index.js
+++ constants/index.js
@@ -8,4 +8,10 @@ export const DEFAULT_UPLOAD = 'https://res.cloudinary.com/nextjs-mongodb-app-btn
 
 export const DEFAULT_POST = 'https://res.cloudinary.com/nextjs-mongodb-app-btn/image/upload/v1655104572/social-media_sljfh2.png';
 
+export const IMG_CONFIG = {
+  width: 600,
+  height: 480,
+  crop: 'fill',
+}
+
 export default {};
